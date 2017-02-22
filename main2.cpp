62d1a819697397e85d4f12e2ed74b5b4b58e8c74
Brian Do <officialbriando@gmail.com>
2017-02-22 13:31:05 -0800

    Adds nextBoardMirrored() for Mirrored boundary mode.

--- main2.cpp
+++ main2.cpp
@@ -13,6 +13,7 @@ void initializeBoard(string file, char**& board);
 
 int nextBoardClassic(int rows, int columns, char**& board);
 int nextBoardTorus(int rows, int columns, char**& board);
+int nextBoardMirrored(int rows, int columns, char**& board);
 void printBoard(int rows, int columns, char** board);
 
 void prepGame(string& file, int& rows, int& columns);
@@ -126,7 +127,7 @@ void startGame(int& rows, int& columns, char**& board){
 	cin.ignore();	cin.ignore();
 	while(stabilized == 0) //Sets the while loop using an indicator for whether the board has stabilized.
 	{
-		stabilized = nextBoardTorus(rows, columns, board); //Integrated method for creating new generations returns a value indicating stabilization.
+		stabilized = nextBoardMirrored(rows, columns, board); //Integrated method for creating new generations returns a value indicating stabilization.
 
 		if(stabilized == 1)
 		{
@@ -341,6 +342,120 @@ int nextBoardTorus(int rows, int columns, char**& board) //Creates a new board w
 				if(board[0][j-1] == 'X') count++;
 				if(board[0][j] == 'X') count++;
 				if(board[0][j+1] == 'X') count++;
+			}
+			else 										
+			{
+				if(board[i+1][j] == 'X') count++;
+				if(board[i][j+1] == 'X') count++;
+				if(board[i+1][j+1] == 'X') count++;
+				if(board[i][j-1] =='X') count++;
+				if(board[i-1][j] == 'X') count++;
+				if(board[i-1][j-1] == 'X') count++;
+				if(board[i+1][j-1] =='X') count++;
+				if(board[i-1][j+1] == 'X') count++;
+			}
+
+			//Changes the next generation's population based on neighbors.
+			if(count < 2) nextGen[i][j] = '-';
+			else if(count == 2) nextGen[i][j] = board[i][j];
+			else if(count == 3) nextGen[i][j] = 'X';
+			else if(count > 3) nextGen[i][j] = '-';
+		}
+	}
+
+	//Checking to see if the next generation is the same as the previous, returns '1' if true, '0' if false.
+	bool stabilized = true;
+
+	for(int i = 0; i < rows; ++i)
+	{
+		for(int j = 0; j < columns; ++j)
+		{
+			if(nextGen[i][j] != board[i][j]) stabilized = false;
+		}
+	}
+
+	if(stabilized)
+	{
+		cout << "The world has stabilized. There are no further changes to the population." << endl;
+		return 1;
+	}
+	else //Copies the next generation over to the primary board.
+	{ 
+		for(int i = 0; i < rows; ++i)
+		{
+			for(int j = 0; j < columns; ++j)board[i][j] = nextGen[i][j];
+		}
+		return 0;
+	}
+}
+
+int nextBoardMirrored(int rows, int columns, char**& board) //Creates a new board with adjustments for next generation, then copies it to the primary board.
+{
+	char** nextGen = new char*[rows]; //Creates a second board based on first board dimensions for copying later.
+	for(int i = 0; i < rows; ++i) nextGen[i] = new char[columns];
+
+	//Creating the next generation based on the previous.
+
+	for(int i = 0; i < rows; ++i){
+		for(int j = 0; j < columns; ++j){
+			int count = 0; 								//Keeps track of amount of neighbors.
+
+			if(i == 0 && j == 0){ 						//All checks necessary to avoid IndexOutOfBounds error.
+				if(board[i+1][j] == 'X') count+=2; 		
+				if(board[i][j+1] == 'X') count+=2;
+				if(board[i+1][j+1] == 'X') count++;
+				if(board[i][j] == 'X') count+=3;	//Begins checking for off grid areas using torus wrapping.
+			}
+			else if(i == 0 && j == (columns - 1)){
+				if(board[i+1][j] == 'X') count+=2;
+				if(board[i][j-1] == 'X') count+=2;
+				if(board[i+1][j-1] == 'X') count++;
+				if(board[i][j] == 'X') count+=3;
+			}
+			else if(i == (rows - 1 ) && j == (columns - 1)){
+				if(board[i-1][j] == 'X') count+=2;
+				if(board[i][j-1] == 'X') count+=2;
+				if(board[i-1][j-1] == 'X') count++;
+				if(board[i][j] == 'X') count+=3;
+			}
+			else if(i == (rows - 1) && j == 0){
+				if(board[i-1][j] == 'X') count+=2;
+				if(board[i][j+1] == 'X') count+=2;
+				if(board[i-1][j+1] == 'X') count++;
+				if(board[i][j] == 'X') count+=3;
+			}
+			else if(j == 0){ 							
+				if(board[i-1][j] == 'X') count+=2;
+				if(board[i+1][j] == 'X') count+=2;
+				if(board[i][j+1] == 'X') count++;
+				if(board[i+1][j+1] == 'X') count++;
+				if(board[i-1][j+1] == 'X') count++;
+				if(board[i][j] == 'X') count++;
+			}
+
+			else if(i == 0){
+				if(board[i+1][j] == 'X') count++;
+				if(board[i][j+1] == 'X') count+=2;
+				if(board[i+1][j+1] == 'X') count++;
+				if(board[i][j-1] =='X') count+=2;
+				if(board[i+1][j-1] == 'X') count++;
+				if(board[i][j] == 'X') count++;
+			}
+			else if(j == (columns - 1)){
+				if(board[i-1][j] == 'X') count+=2;
+				if(board[i+1][j] == 'X') count+=2;
+				if(board[i][j-1] == 'X') count++;
+				if(board[i+1][j-1] == 'X') count++;
+				if(board[i-1][j-1] == 'X') count++;
+				if(board[i][j] == 'X') count++;
+			}
+			else if(i == (rows - 1)){
+				if(board[i-1][j] == 'X') count++;
+				if(board[i][j+1] == 'X') count+=2;
+				if(board[i-1][j+1] == 'X') count++;
+				if(board[i][j-1] =='X') count+=2;
+				if(board[i-1][j-1] == 'X') count++;
+				if(board[i][j] == 'X') count++;
 			}
 			else 										
 			{
